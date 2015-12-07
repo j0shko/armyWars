d429cc2e7a83c65e036d2e0c91c8c077a887b0af
Josip Tomić <josip.tomic@fer.hr>
2015-12-07 17:21:21 +0100

    Initial commit

---
+++ src/main/java/hr/degordian/armyWars/App.java
@@ -0,0 +1,13 @@
+package hr.degordian.armyWars;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
